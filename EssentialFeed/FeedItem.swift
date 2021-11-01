7602c5829ca7ffbba9c1b10c09e3dfa9c70d6c8c
Milan Kamilya <milankamilya@gmail.com>
2021-11-01 09:10:01 +0530

    created feedItem

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by MK on 01/11/21.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
