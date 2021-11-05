874750119762e3782c907b3732a789daf2b9d9d7
Milan Kamilya <milankamilya@gmail.com>
2021-11-05 09:37:56 +0530

    Move mapping logic to new type FeedItemsMapper

--- EssentialFeed/Feed APi/RemoteFeedLoader.swift
+++ EssentialFeed/Feed APi/RemoteFeedLoader.swift
@@ -41,9 +41,11 @@ public final class RemoteFeedLoader{
         client.get(from: url) { result in
             switch result {
             case let .success(data, response):
-                if response.statusCode == 200, let root = try? JSONDecoder().decode(Root.self, from: data) {
-                    completion(.success(root.items.map({ $0.item })))
-                } else {
+                
+                do {
+                    let items = try FeedItemsMapper.map(data, response)
+                    completion(.success(items))
+                } catch {
                     completion(.failure(.invalidData))
                 }
                 
@@ -55,18 +57,31 @@ public final class RemoteFeedLoader{
     }
 }
 
-private struct Root: Decodable {
-    let items: [Item]
-}
-
-private struct Item: Decodable {
-    public let id: UUID
-    public let description: String?
-    public let location: String?
-    public let image: URL
+private class FeedItemsMapper {
     
-    var item: FeedItem {
-        return FeedItem(id: id, description: description, location: location, imageURL: image)
+    private struct Root: Decodable {
+        let items: [Item]
+    }
+
+    private struct Item: Decodable {
+        public let id: UUID
+        public let description: String?
+        public let location: String?
+        public let image: URL
+        
+        var item: FeedItem {
+            return FeedItem(id: id, description: description, location: location, imageURL: image)
+        }
+    }
+    
+    static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [FeedItem] {
+        guard response.statusCode == 200 else {
+            throw RemoteFeedLoader.Error.invalidData
+        }
+        
+        let root = try JSONDecoder().decode(Root.self, from: data)
+        return root.items.map({ $0.item })
     }
 }
+
 
