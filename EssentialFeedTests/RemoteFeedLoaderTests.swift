01ec3de92ff4613597a029c31f0fd86c603a01bc
Milan Kamilya <milankamilya@gmail.com>
2021-11-06 11:04:45 +0530

    Simplify type declarations with a helper func

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -38,7 +38,7 @@ class RemoteFeedLoaderTests: XCTestCase {
     func test_load_deliversErrorOnClientError() {
         let (sut, client) = makeSUT()
         
-        expect(sut, toCompeteWithResult: .failure(RemoteFeedLoader.Error.connectivity)) {
+        expect(sut, toCompeteWithResult: failure(.connectivity)) {
             let clientError = NSError(domain: "Test", code: 0)
             client.complete(with: clientError)
         }
@@ -50,7 +50,7 @@ class RemoteFeedLoaderTests: XCTestCase {
         let samples = [199, 201, 300, 400, 500]
         
         samples.enumerated().forEach { index, code in
-            expect(sut, toCompeteWithResult: .failure(RemoteFeedLoader.Error.invalidData)) {
+            expect(sut, toCompeteWithResult: failure(.invalidData)) {
                 let validateData = makeItemsJSON(items: [])
                 client.complete(withStatusCode: code, data: validateData, at: index)
             }
@@ -60,7 +60,7 @@ class RemoteFeedLoaderTests: XCTestCase {
     func test_load_deliversErrorOn200HTTPResponseWithInvalidJSON() {
         let (sut, client) = makeSUT()
 
-        expect(sut, toCompeteWithResult: .failure(RemoteFeedLoader.Error.invalidData)) {
+        expect(sut, toCompeteWithResult: failure(.invalidData)) {
             let invalidJSON = Data( "invalid json".utf8)
             client.complete(withStatusCode: 200, data: invalidJSON, at: 0)
         }
@@ -121,6 +121,10 @@ class RemoteFeedLoaderTests: XCTestCase {
         trackForMemoryLeaks(sut, file: file, line: line)
         trackForMemoryLeaks(client, file: file, line: line)
         return (sut, client)
+    }
+    
+    private func failure(_ error: RemoteFeedLoader.Error) -> RemoteFeedLoader.Result {
+        return .failure(error)
     }
     
     private func trackForMemoryLeaks(_ instance: AnyObject, file: StaticString = #filePath, line: UInt = #line) {
