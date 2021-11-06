7df65726b920aecccc8664304d0313a06377b538
Milan Kamilya <milankamilya@gmail.com>
2021-11-06 06:29:06 +0530

    HTTPClient & FeedItemMapper put into separate files under Feed API

---
+++ EssentialFeed/Feed APi/HTTPClient.swift
@@ -0,0 +1,17 @@
+//
+//  HTTPClient.swift
+//  EssentialFeed
+//
+//  Created by MK on 06/11/21.
+//
+
+import Foundation
+
+public enum HTTPClientResult {
+    case success(Data, HTTPURLResponse)
+    case error(Error)
+}
+
+public protocol HTTPClient {
+    func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void)
+}
--- EssentialFeed/Feed APi/RemoteFeedLoader.swift
+++ EssentialFeed/Feed APi/RemoteFeedLoader.swift
@@ -9,15 +9,6 @@ import Foundation
 
 public typealias CommonCompletionType = (Error?, HTTPURLResponse?) -> Void
 
-public enum HTTPClientResult {
-    case success(Data, HTTPURLResponse)
-    case error(Error)
-}
-
-public protocol HTTPClient {
-    func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void)
-}
-
 public final class RemoteFeedLoader{
     private let url: URL
     private let client: HTTPClient
@@ -56,34 +47,3 @@ public final class RemoteFeedLoader{
         
     }
 }
-
-private class FeedItemsMapper {
-    
-    private struct Root: Decodable {
-        let items: [Item]
-    }
-
-    private struct Item: Decodable {
-        public let id: UUID
-        public let description: String?
-        public let location: String?
-        public let image: URL
-        
-        var item: FeedItem {
-            return FeedItem(id: id, description: description, location: location, imageURL: image)
-        }
-    }
-    
-    static var OK_200: Int { return 200 }
-    
-    static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [FeedItem] {
-        guard response.statusCode == OK_200 else {
-            throw RemoteFeedLoader.Error.invalidData
-        }
-        
-        let root = try JSONDecoder().decode(Root.self, from: data)
-        return root.items.map({ $0.item })
-    }
-}
-
-
