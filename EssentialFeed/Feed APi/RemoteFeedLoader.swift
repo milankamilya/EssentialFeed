0725a6caf6254ec9f42b2e314eff906ca0975459
Milan Kamilya <milankamilya@gmail.com>
2021-11-06 07:02:48 +0530

    Guarentee that we don't deliver result after `RemoteFeedLoader` instance has been deallocated.

--- EssentialFeed/Feed APi/RemoteFeedLoader.swift
+++ EssentialFeed/Feed APi/RemoteFeedLoader.swift
@@ -29,7 +29,8 @@ public final class RemoteFeedLoader{
     }
     
     public func load(completion: @escaping (Result)-> Void) {
-        client.get(from: url) { result in
+        client.get(from: url) { [weak self] result in
+            guard let _ = self else { return }
             switch result {
             case let .success(data, response):
                 completion(FeedItemsMapper.map(data, response))
