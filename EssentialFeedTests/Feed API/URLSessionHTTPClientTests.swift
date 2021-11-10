31c619d1a55b3dd3763a0fa4edcfa493c82322ae
Milan Kamilya <milankamilya@gmail.com>
2021-11-10 05:55:41 +0530

    Delivers data & response on successful http url response

--- EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -18,9 +18,11 @@ class URLSessionHTTPClient {
     struct UnexpectedvaluesRepresentation: Error { }
     
     func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
-        session.dataTask(with: url) { _, _, error in
+        session.dataTask(with: url) { data, response, error in
             if let error = error {
                 completion(.failure(error))
+            } else if let data = data, data.count > 0, let response = response as? HTTPURLResponse {
+                completion(.success(data, response))
             } else {
                 completion(.failure(UnexpectedvaluesRepresentation()))
             }
@@ -80,6 +82,30 @@ class URLSessionHTTPClientTests: XCTestCase {
         XCTAssertNotNil( resultErrorFor(data: anyData(), response: nonHTTPURLResponse(), error: anyNSError()) )
         XCTAssertNotNil( resultErrorFor(data: anyData(), response: anyHTTPURLResponse(), error: anyNSError()) )
         XCTAssertNotNil( resultErrorFor(data: anyData(), response: nonHTTPURLResponse(), error: nil) )
+    }
+    
+    func test_getFromURL_succeedsOnHTTPURLResponseWIthData() {
+        let data = anyData()
+        let response = anyHTTPURLResponse()
+        URLProtocolStub.stub(data: data, response: response, error: nil)
+        
+        let exp = expectation(description: "Wait for completion")
+        
+        makeSUT().get(from: anyURL()) { result in
+            switch result {
+            case let .success(receivedData, receivedResponse):
+                XCTAssertEqual(receivedData, data)
+                XCTAssertEqual(receivedResponse.url, response.url)
+                XCTAssertEqual(receivedResponse.statusCode, response.statusCode)
+            default:
+                XCTFail("Expected success, got \(result) instead")
+            }
+            
+            exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 1.0)
+        
     }
     
     // MARK: - Helpers
