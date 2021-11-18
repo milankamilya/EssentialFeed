1887f0aba625a52830341d2d8134988b9f039148
Milan Kamilya <milankamilya@gmail.com>
2021-11-18 07:18:58 +0530

    Add empty `CoreDataFeedStoreTests`

---
+++ EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
@@ -0,0 +1,84 @@
+//
+//  CoreDataFeedStoreTests.swift
+//  EssentialFeedTests
+//
+//  Created by MK on 18/11/21.
+//
+
+import XCTest
+
+class CoreDataFeedStoreTests: XCTestCase, FailableFeedStore {
+    
+    func test_retrieve_deliversEmptyOnEmptyCache() {
+        
+    }
+    
+    func test_retrieve_hasNoSideOnEmptyCache() {
+        
+    }
+    
+    func test_retrieve_deliversFoundValueOnNonEmptyCache() {
+        
+    }
+    
+    func test_retrieve_hasNoSideEffectsOnNonEmptyCache() {
+        
+    }
+    
+    func test_retrieve_deliversErrorOnInvalidCache() {
+        
+    }
+    
+    func test_retrieve_hasNoSideEffectsOnInvalidCache() {
+        
+    }
+    
+    func test_insert_deliversNoErrorOnEmptyCache() {
+        
+    }
+    
+    func test_insert_deliversNoErrorOnNonEmptyCache() {
+        
+    }
+    
+    func test_insert_overridePreviouslyInsertedValues() {
+        
+    }
+    
+    func test_insert_deliversErrorOnInsertionError() {
+        
+    }
+    
+    func test_insert_hasNoSideEffectsOnInsertionError() {
+        
+    }
+    
+    func test_delete_deliversNoErrorOnEmptyCache() {
+        
+    }
+    
+    func test_delete_hasNoEffectOnEmptyCache() {
+        
+    }
+    
+    func test_delete_deliversNoErrorOnNonEmptyCache() {
+        
+    }
+    
+    func test_delete_leavesEmptyCacheOnNonEmptyCache() {
+        
+    }
+    
+    func test_delete_deliverErrorOnDeletionError() {
+        
+    }
+    
+    func test_delete_hasNoSideEffectsOnDeletionError() {
+        
+    }
+    
+    func test_storeSideEffects_runSerially() {
+        
+    }
+    
+}
