8e04b4172a18b3fadc13d69f47889ed8aed7356b
Milan Kamilya <milankamilya@gmail.com>
2021-11-18 05:55:45 +0530

    Add `XCTestCase+FailableDeletionFeedStoreSpecs`

---
+++ EssentialFeedTests/Feed Cache/FeedStoreSpecs/XCTestCase+FailableDeletionFeedStoreSpecs.swift
@@ -0,0 +1,13 @@
+//
+//  XCTestCase+FailableDeletionFeedStoreSpecs.swift
+//  EssentialFeedTests
+//
+//  Created by MK on 18/11/21.
+//
+
+import Foundation
+import XCTest
+
+extension FailableDeletionFeedStoreSpecs where Self: XCTestCase {
+    
+}
