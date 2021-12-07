79faa999ef3015297e0556af3ee26618aad121fe
Milan Kamilya <milankamilya@gmail.com>
2021-12-07 08:04:04 +0530

    Extract `FeedLoadingViewModel` & `FeedViewModel` into their respective independent files

--- EssentialFeed/EssentialFeediOS/Feed UI/Feed Presentation/FeedPresenter.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Feed Presentation/FeedPresenter.swift
@@ -8,17 +8,10 @@
 import Foundation
 import EssentialFeed
 
-struct FeedLoadingViewModel {
-    let isLoading: Bool
-}
-
 protocol FeedLoadingView {
     func display(_ viewModel: FeedLoadingViewModel)
 }
 
-struct FeedViewModel {
-    let feed: [FeedImage]
-}
 protocol FeedView {
     func display(_ viewModel: FeedViewModel)
 }
