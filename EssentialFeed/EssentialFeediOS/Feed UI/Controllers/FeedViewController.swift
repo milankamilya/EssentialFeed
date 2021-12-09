6959475c8a25e62a73719def3f7608b4c04b1ee4
Milan Kamilya <milankamilya@gmail.com>
2021-12-09 07:43:48 +0530

    Set `FeedViewController` title

--- EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
@@ -24,6 +24,7 @@ final public class FeedViewController: UITableViewController, UITableViewDataSou
     public override func viewDidLoad() {
         super.viewDidLoad()
         
+        title = "My Feed"
         refresh()
     }
     
