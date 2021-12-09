d2ddec43ca6ce9ac2eb47aeae84fc0251bb52f0e
Milan Kamilya <milankamilya@gmail.com>
2021-12-09 06:17:56 +0530

    Instantiate `FeedViewController` from Feed storyboard

--- EssentialFeed/EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
@@ -15,7 +15,11 @@ final public class FeedUIComposer {
 
         let presentationAdapter = FeedLoaderPresentationAdapter(loader: feedLoader)
         let refreshController = FeedRefreshViewController(delegate: presentationAdapter)
-        let feedController = FeedViewController(refreshController: refreshController)
+        
+        let bundle = Bundle(for: FeedViewController.self)
+        let storyboard = UIStoryboard(name: "Feed", bundle: bundle)
+        let feedController = storyboard.instantiateInitialViewController() as! FeedViewController
+        feedController.refreshController = refreshController
         
         let presenter = FeedPresenter(
             loadingView: WeakRefVirtualProxy(refreshController),
