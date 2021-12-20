f0487fe08f05dcd278d277721abe497794c1c554
Milan Kamilya <milankamilya@gmail.com>
2021-12-20 08:00:07 +0530

    Extract system under test creation to a test helper method

--- EssentialFeed/EssentialFeedTests/Feed Presentation/FeedPresenterTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Presentation/FeedPresenterTests.swift
@@ -16,14 +16,20 @@ private final class FeedPresenter {
 
 final class FeedPresenterTests : XCTestCase {
     func test_init_doesNotSendAnyMessagesToView() {
-        let view = ViewSpy()
-        
-        _ = FeedPresenter(view: view)
+        let (_, view) = makeSUT()
         
         XCTAssertTrue(view.messages.isEmpty, "Expected not to receive any messages, but received \(view.messages) instead.")
     }
     
     // MARK: - Helpers
+    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (sut: FeedPresenter, view: ViewSpy) {
+        let view = ViewSpy()
+        let sut = FeedPresenter(view: view)
+        trackForMemoryLeaks(view)
+        trackForMemoryLeaks(sut)
+        return (sut, view)
+    }
+    
     private class ViewSpy {
         let messages = [Any]()
     }
