e50148c6782b1c8f8865647663076dcd87e04a66
Milan Kamilya <milankamilya@gmail.com>
2021-12-22 09:08:58 +0530

    `FeedImagePresenter.didStartLoadingImageData(model:)` invokes view's delegate method with initial view model

--- EssentialFeed/EssentialFeedTests/Feed Presentation/FeedImagePresenterTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Presentation/FeedImagePresenterTests.swift
@@ -7,24 +7,87 @@
 
 import Foundation
 import XCTest
+@testable import EssentialFeed
 
-private final class FeedImagePresenter {
-    init() {
-        
+
+struct FeedImageViewModel<Image> {
+    let image: Image?
+    let description: String?
+    let location: String?
+    let isLoading: Bool
+    let shouldRetry: Bool
+    
+    var hasLocation: Bool {
+        return location != nil
+    }
+}
+
+protocol FeedImageView {
+    associatedtype Image
+    
+    func display(_ viewModel: FeedImageViewModel<Image>)
+}
+
+private final class FeedImagePresenter<View: FeedImageView, Image> where View.Image == Image {
+    private let view: View
+    
+    init(view: View) {
+        self.view = view
+    }
+    
+    func didStartLoadingImageData(model: FeedImage) {
+        view.display(FeedImageViewModel<Image>(
+            image: nil,
+            description: model.description,
+            location: model.location,
+            isLoading: true,
+            shouldRetry: false
+        ))
     }
 }
 
 final class FeedImagePresenterTests: XCTestCase {
     func test_init_doesNotSendAnyMessage() {
         let view = ViewSpy()
-        _ = FeedImagePresenter()
+        _ = FeedImagePresenter(view: view)
         
         XCTAssertTrue(view.messages.isEmpty, "Expected init doesn't send any message, but sent \(view.messages) instead")
     }
     
-    // MARK: - Helpers
-    
-    private class ViewSpy {
-        let messages = [Any]()
+    func test_didStartLoadingImageData_displayTextAndStartLoading() {
+        let view = ViewSpy()
+        let sut = FeedImagePresenter(view: view)
+        let feedImage = uniqueImage()
+        let expectedFeedImageViewModel = FeedImageViewModel<ViewSpy.Image>(image: nil, description: feedImage.description, location: feedImage.location, isLoading: true, shouldRetry: false)
+        
+        sut.didStartLoadingImageData(model: feedImage)
+        
+        XCTAssertEqual(view.messages, [.display(expectedFeedImageViewModel)])
     }
+    
+    // MARK: - Helpers
+    private class ImageStub : Equatable {
+        static func == (lhs: FeedImagePresenterTests.ImageStub, rhs: FeedImagePresenterTests.ImageStub) -> Bool {
+            return true
+        }
+    }
+    
+    private class ViewSpy: FeedImageView {
+        
+        typealias Image = ImageStub
+        
+        enum Message: Equatable {
+            case display(FeedImageViewModel<Image>)
+        }
+
+        var messages = [Message]()
+        
+        func display(_ viewModel: FeedImageViewModel<Image>) {
+            messages.append(.display(viewModel))
+        }
+    }
+}
+
+extension FeedImageViewModel : Equatable where Image : Equatable {
+    
 }
