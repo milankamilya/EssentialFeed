7ff6772c02038a7af597f4a347cee5a36f85eeba
Milan Kamilya <milankamilya@gmail.com>
2022-02-02 08:32:08 +0530

    `LocalFeedImageDataLoader.loadImageData` requests stored data for URL from `FeedImageDataStore`

--- EssentialFeed/EssentialFeedTests/Feed Cache/LocalFeedImageDataLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/LocalFeedImageDataLoaderTests.swift
@@ -6,18 +6,44 @@
 //
 
 import XCTest
+import EssentialFeed
+
+protocol FeedImageDataStore {
+    func retrieve(dataForURL: URL)
+}
 
 private class LocalFeedImageDataLoader {
-    init(store: Any) {
+    private struct Task: FeedImageDataLoaderTask {
+        func cancel() {}
+    }
+    private let store: FeedImageDataStore
+    
+    init(store: FeedImageDataStore) {
+        self.store = store
+    }
+    
+    func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
         
+        store.retrieve(dataForURL: url)
+        return Task()
     }
 }
 
 class LocalFeedImageDataLoaderTests: XCTestCase {
+    
     func test_doesNotMessageStoreOnCreation() {
         let (_, store) = makeSUT()
         
         XCTAssertTrue(store.receivedMessages.isEmpty)
+    }
+    
+    func test_loadImageDataFromURL_requestsStoredDataForURL() {
+        let (sut, store) = makeSUT()
+        let url = anyURL()
+        
+        _ = sut.loadImageData(from: url) { _ in }
+        
+        XCTAssertEqual(store.receivedMessages, [.retrieve(dataForURL: url)])
     }
     
     // MARK: - Helpers
@@ -32,7 +58,14 @@ class LocalFeedImageDataLoaderTests: XCTestCase {
         return (sut, store)
     }
     
-    private class FeedStoreSpy {
-        var receivedMessages = [Any]()
+    private class FeedStoreSpy: FeedImageDataStore {
+        enum Message: Equatable {
+            case retrieve(dataForURL: URL)
+        }
+        var receivedMessages = [Message]()
+        
+        func retrieve(dataForURL url: URL) {
+            receivedMessages.append(.retrieve(dataForURL: url))
+        }
     }
 }
