fc6315215c6b1dd68f6cbdb9edfbad561dc14de8
Milan Kamilya <milankamilya@gmail.com>
2022-02-11 08:52:46 +0530

    Clean up references to persistent stores on `CoreDataFeedStore.deinit` to encapsulate the whole CoreData stack lifecycle within the `CoreDataFeedStore` instance life time

--- EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/CoreDataFeedStore.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/CoreDataFeedStore.swift
@@ -9,17 +9,28 @@ import Foundation
 import CoreData
 
 public final class CoreDataFeedStore {
-    private let persistanceContainer: NSPersistentContainer
+    private let container: NSPersistentContainer
     private let context: NSManagedObjectContext
     
     public init(storeURL url: URL) throws {
         let bundle = Bundle(for: CoreDataFeedStore.self)
-        persistanceContainer = try NSPersistentContainer.load(modelName: "FeedStore", url: url, in: bundle)
-        context = persistanceContainer.newBackgroundContext()
+        container = try NSPersistentContainer.load(modelName: "FeedStore", url: url, in: bundle)
+        context = container.newBackgroundContext()
     }
     
     func perform(_ action: @escaping (NSManagedObjectContext) -> Void) {
         let context = self.context
         context.perform { action(context) }
+    }
+    
+    private func cleanUpReferencesToPersistentStores() {
+        context.performAndWait {
+            let coordinator = self.container.persistentStoreCoordinator
+            try? coordinator.persistentStores.forEach(coordinator.remove)
+        }
+    }
+    
+    deinit {
+        cleanUpReferencesToPersistentStores()
     }
 }
