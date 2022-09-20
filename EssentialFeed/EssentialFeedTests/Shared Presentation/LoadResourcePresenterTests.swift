4415ced794c7795c544e3e7e8f4455e2942ec3ac
Milan Kamilya <milankamilya@gmail.com>
2022-09-20 08:15:15 +0530

    Remove title from generic presenter since it specific to modules

--- EssentialFeed/EssentialFeedTests/Shared Presentation/LoadResourcePresenterTests.swift
+++ EssentialFeed/EssentialFeedTests/Shared Presentation/LoadResourcePresenterTests.swift
@@ -10,10 +10,6 @@ import EssentialFeed
 
 class LoadResourcePresenterTests: XCTestCase {
     
-    func test_title_localized() {
-        XCTAssertEqual(LoadResourcePresenter.title, localized("FEED_VIEW_TITLE"))
-    }
-    
     func test_init_doesNotSendAnyMessagesToView() {
         let (_, view) = makeSUT()
         
