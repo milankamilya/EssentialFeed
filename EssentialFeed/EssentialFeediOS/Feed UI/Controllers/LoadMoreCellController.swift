51f25ef7ca18490f6d2997923c86029f5147b75c
Milan Kamilya <milankamilya@gmail.com>
2022-10-18 07:19:20 +0530

    Add `Load more` message label

--- EssentialFeed/EssentialFeediOS/Feed UI/Controllers/LoadMoreCellController.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Controllers/LoadMoreCellController.swift
@@ -20,7 +20,11 @@ public class LoadMoreCellController: NSObject, UITableViewDataSource {
     }
 }
 
-extension LoadMoreCellController: ResourceLoadingView {
+extension LoadMoreCellController: ResourceLoadingView, ResourceErrorView {
+    public func display(_ viewModel: EssentialFeed.ResourceErrorViewModel) {
+        cell.message = viewModel.message
+    }
+    
     public func display(_ viewModel: EssentialFeed.ResourceLoadingViewModel) {
         cell.isLoading = viewModel.isLoading
     }
--- EssentialFeed/EssentialFeediOS/Feed UI/Views/LoadMoreCell.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Views/LoadMoreCell.swift
@@ -20,6 +20,25 @@ public class LoadMoreCell: UITableViewCell {
         return spinner
     }()
     
+    private lazy var messageLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .tertiaryLabel
+        label.font = .preferredFont(forTextStyle: .footnote)
+        label.numberOfLines = 0
+        label.textAlignment = .center
+
+        contentView.addSubview(label)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            label.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            contentView.trailingAnchor.constraint(equalTo: label.trailingAnchor),
+            label.topAnchor.constraint(equalTo: contentView.topAnchor),
+            contentView.bottomAnchor.constraint(equalTo: label.bottomAnchor),
+        ])
+        return label
+    }()
+    
     public var isLoading: Bool {
         get { spinner.isAnimating }
         set {
@@ -29,5 +48,10 @@ public class LoadMoreCell: UITableViewCell {
                 spinner.stopAnimating()
             }
         }
+    }
+    
+    public var message: String? {
+        get { messageLabel.text }
+        set { messageLabel.text = newValue }
     }
 }
