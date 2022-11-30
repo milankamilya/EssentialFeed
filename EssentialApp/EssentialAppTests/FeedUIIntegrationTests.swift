508fe82e3a6a79468d1385849090d90b0a2e7e25
Milan Kamilya <milankamilya@gmail.com>
2022-11-30 09:40:18 +0530

    Fix text cases for load more feed

--- EssentialApp/EssentialAppTests/FeedUIIntegrationTests.swift
+++ EssentialApp/EssentialAppTests/FeedUIIntegrationTests.swift
@@ -128,7 +128,7 @@ class FeedUIIntegrationTests: XCTestCase {
         assertThat(sut, isRendering: [image0, image1])
         
         sut.simulateLoadMoreFeedAction()
-        loader.completeFeedLoading(with: [image0, image1, image2, image3], at: 0)
+        loader.completeLoadMore(with: [image0, image1, image2, image3], at: 0)
         assertThat(sut, isRendering: [image0, image1, image2, image3])
         
         sut.simulateuserInitiatedReload()
@@ -147,7 +147,7 @@ class FeedUIIntegrationTests: XCTestCase {
         assertThat(sut, isRendering: [image0])
         
         sut.simulateLoadMoreFeedAction()
-        loader.completeFeedLoading(with: [image0, image1], at: 0)
+        loader.completeLoadMore(with: [image0, image1], at: 0)
         assertThat(sut, isRendering: [image0, image1])
         
         sut.simulateuserInitiatedReload()
